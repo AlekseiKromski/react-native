019c90d2e4665f784d0406b4e2bc264578607827
AlekseiKromski <aleksei.kromski@outlook.com>
2022-05-11 13:29:17 +0300

    Add todo app and add alert

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import { Navbar } from './src/Navbar';
+import {AddTodo} from './src/AddTodo'
+import {ToDo} from './src/ToDo'
+
+import {useState} from 'react';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    const newTodo = {
+      id: Date.now().toString(),
+      title: title,
+    }
+
+    setTodos((prevState) => {
+      console.log(prevState)
+      return [
+        newTodo,
+        ...prevState
+        
+      ]
+    })
+
+    console.log(todos);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title="Todo app"></Navbar>
+
+      <View style={styles.container}>
+
+        <AddTodo onSubmit={addTodo}></AddTodo>
+        <View>
+          {
+            todos.map(todo => <ToDo key={todo.id} todo={todo}/>)
+          }
+        </View>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingHorizontal: 30
+  }
 });
